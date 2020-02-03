357852e9abb61e57d5edeaded14e01f3523a9b81
fattylego <mhkhong@gmail.com>
2020-02-03 12:08:27 +0800

    First time using github
    Upload all the code except the node_module

---
+++ build/precache-manifest.eb6b1772827cf06a2c7f7981b1c89580.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847d02ffaadbc95be2dbb63705e88b9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901299f0e639f479cedb",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "00bf190469d4cf7a2809",
+    "url": "/static/js/2.b3017c8a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b3017c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901299f0e639f479cedb",
+    "url": "/static/js/main.92546fcc.chunk.js"
+  },
+  {
+    "revision": "65c707f5eef163711fba",
+    "url": "/static/js/runtime-main.7b7feece.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
